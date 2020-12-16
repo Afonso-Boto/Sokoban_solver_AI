968792ec1f4f3ceb1849b4885146df9166e02526
Afonso Boto <afonso.boto@ua.pt>
2020-12-16 22:13:43 +0000

    novo branch

--- tree_search.py
+++ tree_search.py
@@ -3,8 +3,10 @@ from utils import *
 import asyncio
 
 GOAL_COST = 1
+#CLEARED_POS = 4
 FLOOR_COST = 2
 KEEPER_MOVE_COST = 3
+
 #DEADLOCK_COST = 50
 
 
@@ -43,9 +45,8 @@ class SokobanSolver:
         self.deadlocks = []
         self.strategy = strategy
         self.method = method
-        self.deadlocks_pos = []
-    
-    
+        self.deadlocks_pos = [(p_x, p_y) for p_x in range(level_map.hor_tiles) for p_y in range(level_map.hor_tiles) if self.isDeadlock((p_x, p_y))]
+        print(self.deadlocks_pos)   
     # obtain the path from the initial state to the goal state
     def get_path(self,node):
         if node.parent == None:
@@ -55,9 +56,6 @@ class SokobanSolver:
         
         return path
     
-    #def has_path(self, BoxPos):
-    #    return self.get_path(node) != []
-
     def result(self, current_state, direction):
         '''
             RECEIVES: the current state and a direction (action)
@@ -70,7 +68,6 @@ class SokobanSolver:
         '''
         next_state = calc_next_state(current_state, direction)
         return next_state
-    
     
     def actions(self, current_state):
         '''
@@ -87,27 +84,36 @@ class SokobanSolver:
         for direction in DIRECTIONS:
             next_state = calc_next_state(current_state,direction)
             keeper = next_state['keeper']
-            boxes = next_state['boxes'][:]
+            boxes = next_state['boxes']
             
             valid_directions.append(direction)
-            if Map.is_blocked(self.level_map,keeper):
-                valid_directions.remove(direction)
+
             
-            # Check wether we are placing a box on top of another            
-            if len(list(set(boxes))) < len (boxes):
+            # Check wether we are placing a box on top of another
+            # if direction in valid_directions:            
+            if len(list(set(boxes))) < len (boxes) or Map.is_blocked(self.level_map,keeper):
                 valid_directions.remove(direction)
-            
-            for box in boxes:
-                ''' Estas verificações não poupam grande coisa em termo de nós abertos '''
-                if box in self.deadlocks_pos:
-                    valid_directions.remove(direction)                    
-                elif self.isDeadlock(box):
-                    self.deadlocks_pos.append(box)                
-                    self.deadlocks.append(next_state)
-                    valid_directions.remove(direction)
+                continue
+
+            if valid_directions != []:
+                for box in boxes:
+                    ''' Estas verificações não poupam grande coisa em termo de nós abertos '''
+                    if box in self.deadlocks_pos:
+                        if next_state not in self.deadlocks:
+                            self.deadlocks.append(next_state)
+                        if direction in valid_directions:
+                            valid_directions.remove(direction)
+                        continue
+                        
+                                            
+                    elif self.isDeadlock(box, current_state['goals']):
+                        print("DEADLOCK FOUND, POS =", box)
+                        self.deadlocks_pos.append(box)                
+                        self.deadlocks.append(next_state)
+                        valid_directions.remove(direction)
+                    
         return list(set(valid_directions))
     
-
     def heuristic(self, current_state,cost=3,method='manhatan'):
         keeper = current_state['keeper']
         boxes = current_state['boxes']
@@ -116,7 +122,6 @@ class SokobanSolver:
         for box in boxes:
             heuristic += calc_distance(box,goals,method)
         return heuristic*cost
-        
         
     def cost(self, current_state, direction):
         ''' 
@@ -128,24 +133,20 @@ class SokobanSolver:
             -> Calculates the next state (positions of the keeper and boxes)
             -> Returns the cost of achieving the new state
         '''
-        prev_boxes = current_state['boxes'][:]
+        prev_boxes = current_state['boxes']
         next_state = calc_next_state(current_state, direction)
             # check if the next position is a goal
         
-        boxes = next_state['boxes'][:]
+        boxes = next_state['boxes']
         for box in boxes:
             if box in self.goals_position:
                 return GOAL_COST
                 # check if the next position is a deadlock
-            
-            #isto acho que nao faz nada, nunca vemos o custo de um estado se ele for um deadlock
-            #elif box in self.deadlocks_pos:
-            #    return DEADLOCK_COST
-        
+                #    
         # if we moved a box into a normal floor tile    
         boxes.sort()
         prev_boxes.sort()
-        if str(boxes) != str(prev_boxes):
+        if boxes != prev_boxes:
             return FLOOR_COST
         
         #if its neither a goal, a deadlock nor moved a box return the cost of a keeper move
@@ -164,11 +165,11 @@ class SokobanSolver:
         current_state['boxes'].sort()
         current_state['goals'].sort()
         return current_state['boxes'] == current_state['goals']
-
     # procurar a solucao
     async def search(self, state):
         #permite inicializar uma nova arvore de cada vez que é chamada a funcao search
         #faz reset basicamente
+        
         root = SearchNode(state,None,cost=0,heuristic=self.heuristic(current_state=state,method=self.method))
         self.open_nodes = [root]
         open_nodes = 0
@@ -204,7 +205,6 @@ class SokobanSolver:
                     lnewnodes.append(new_node)
             self.add_to_open(lnewnodes)
         return None
-
     # juntar novos nos a lista de nos abertos de acordo com a estrategia
     def add_to_open(self,lnewnodes):
         if self.strategy == 'breadth':
@@ -218,20 +218,38 @@ class SokobanSolver:
         elif self.strategy == 'a*':
             self.open_nodes.extend(lnewnodes)
             self.open_nodes.sort(key=lambda node: (node.cost + node.heuristic))
-
     # auxiliary method for calculating deadlocks
-    def isDeadlock(self, pos):
+    def isDeadlock(self, pos, goals_position=[]):
         i_x = 0 #number of horizontal wall next to the pos i
         i_y = 0 #number of vertical wall next to the pos i
+        aux = False
         other_boxes = [box for box in self.boxes_position if box != pos]
         if self.level_map.is_blocked(pos):
             return True
-        if self.level_map.is_blocked((pos[0] + 1, pos[1])) or self.level_map.is_blocked((pos[0] - 1, pos[1])) or (pos[0] + 1, pos[1]) in other_boxes or (pos[0] - 1, pos[1]) in other_boxes:
+        if self.level_map.is_blocked((pos[0] + 1, pos[1])) or self.level_map.is_blocked((pos[0] - 1, pos[1])): 
             i_x += 1
-        if self.level_map.is_blocked((pos[0], pos[1] + 1)) or self.level_map.is_blocked((pos[0], pos[1] - 1)) or (pos[0], pos[1] + 1) in other_boxes or (pos[0], pos[1] - 1) in other_boxes:
+        if self.level_map.is_blocked((pos[0], pos[1] + 1)) or self.level_map.is_blocked((pos[0], pos[1] - 1)):
             i_y += 1
 
-        if (i_x > 0 and i_y > 0) and pos in self.goals_position: # verifies if is not on a corner and if it is, make sure it's not a goal
+        if (i_x > 0 and i_y > 0) and pos not in goals_position: # verifies if is not on a corner and if it is, make sure it's not a goal
            return True
+        
+        '''for gp in goals_position:
+            if(pos[0] > gp[0] and self.level_map.is_blocked((pos[0] + 1, pos[1]))) or (pos[0] < gp[0] and self.level_map.is_blocked((pos[0] - 1, pos[1]))):
+                aux = True
+            else:
+                aux = False
+
+            if(pos[1] > gp[1] and self.level_map.is_blocked((pos[0], pos[1] + 1))) or (pos[1] < gp[1] and self.level_map.is_blocked((pos[0] , pos[1] - 1))):
+                aux = True
+            else:
+                aux = False
+        '''
+        return aux
+    
+    def is_cleared_pos(self, next_state):
+        for box in next_state['boxes']:
+            if not (self.isDeadlock((box[0] + 1, box[1])) or self.isDeadlock((box[0] - 1, box[1])) or self.isDeadlock((box[0], box[1] + 1)) or self.isDeadlock((box[0], box[1] - 1))):
+                return True
 
         return False
